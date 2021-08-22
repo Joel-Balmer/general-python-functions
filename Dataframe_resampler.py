4822615c06d062430abcf5e4562c394314dbd8c5
Joel Balmer <joel.balmer@outlook.com>
2021-08-23 11:00:44 +1200

    Function can now resample a table in both x and y directions (down columns and across rows), hence commit. However, currently the function must resample in x with y being optional. I need to modify to make x optional as well.

--- Dataframe_resampler.py
+++ Dataframe_resampler.py
@@ -66,19 +66,22 @@ def dataframe_resampler(df, dx, x_min = None, x_max = None, Y = None, dy = None,
 
     """
     
-    if x_min == None: x_min = df.iat[0,0]
-    if x_max == None: x_max = df.iat[-1,0]
-        
-    X = df.iloc[:,0].values                      # an array of the original df x data column
-    Xn = np.arange(x_min, x_max+dx, dx)          # an array whose elements become the row look up values in the new table ie the new X (Xn) data range
-    # The below array contains the indices such that 0 < (Xn[indices_of_Xn_above_X] - X) < dx in other words, if X and Xn were merged, the indices of X in
-    # the merged array would be indices_of_Xn_above_X - 1
-    indices_of_Xn_above_X = np.ceil((X - X[0])/dx).astype(int)   # ie Xn[indices_of_X_in_Xn] = X
+    # Performing input checks
+    if dy == None and (y_min != None or y_max != None):
+        raise Exception("y_min or y_max was passed, but the step size dy was not.")
     
-    Zn = np.zeros((len(Xn),df.shape[1]-1))               # an array to store the new resampled, dependent variable Z data
+    
+    X = df.iloc[:,0].values                      # an array of the original df x data column
+    if x_min == None: x_min = X[0]
+    if x_max == None: x_max = X[-1]
+    Xn = np.arange(x_min, x_max+dx, dx)          # an array whose elements become the row look up values in the new table, where the n significes 'new' X
+    # The below array contains the indices such that 0 < (Xn[indices_of_Xn_equal_or_above_X] - X) < dx in other words, if X and Xn were merged, the indices of X in
+    # the merged array would be indices_of_Xn_equal_or_above_X - 1
+    indices_of_Xn_equal_or_above_X = np.ceil((X - X[0])/dx).astype(int)
+    Znx = np.zeros((len(Xn),df.shape[1]-1))               # an array to store the new dependent variable Z data after resampling has occured in the x direction (down the columns)
    
-    # Interpolating and/or extrapolating in x (down columns)
-    for i in range(0,Zn.shape[1]):            # forloop to step through the columns of Z data in the original dataframe
+    # Interpolating and/or extrapolating in x (down each columns)
+    for i in range(0,Znx.shape[1]):            # forloop to step through the columns of Z data in the original dataframe
     
         Z = df.iloc[:,i+1].values                # an array of the original df z data for the column of interest
                
@@ -89,24 +92,51 @@ def dataframe_resampler(df, dx, x_min = None, x_max = None, Y = None, dy = None,
         
         # Extrapolating
         if x_min < df.iat[0,0]:
-            Zn[0:indices_of_Xn_above_X[0],i] = dz_dx[0]*Xn[0:indices_of_Xn_above_X[0]]+c[0]
+            Znx[0:indices_of_Xn_equal_or_above_X[0],i] = dz_dx[0]*Xn[0:indices_of_Xn_equal_or_above_X[0]]+c[0]
         if x_max > df.iat[-1,0]:
-            Zn[indices_of_Xn_above_X[-1]:,i] = dz_dx[-1]*Xn[indices_of_Xn_above_X[-1]:]+c[-1]
+            Znx[indices_of_Xn_equal_or_above_X[-1]:,i] = dz_dx[-1]*Xn[indices_of_Xn_equal_or_above_X[-1]:]+c[-1]
         
         elif x_max == df.iat[-1,0]:
-            Zn[-1,i] = df.iat[-1,i+1]
+            Znx[-1,i] = df.iat[-1,i+1]
             
         # Interpolating
         for j in range(0,len(dz_dx)):
-            Zn[indices_of_Xn_above_X[j]:indices_of_Xn_above_X[j+1],i] = dz_dx[j]*Xn[indices_of_Xn_above_X[j]:indices_of_Xn_above_X[j+1]]+c[j]
+            Znx[indices_of_Xn_equal_or_above_X[j]:indices_of_Xn_equal_or_above_X[j+1],i] = dz_dx[j]*Xn[indices_of_Xn_equal_or_above_X[j]:indices_of_Xn_equal_or_above_X[j+1]]+c[j]
              
+    # Interpolating and/or extrapolating in y (along each row)      
+    if dy != None:
+        if Y == None:
+            if all(isinstance(i,float) or isinstance(i,int) for i in df.columns[1:]):
+                Y = df.columns[1:].values
+            else:
+                raise Exception("The columns headers for df are not all floats or ints. Thus (per the docs), the Y input must be used to pass the values associated with each column in the original dataframe.")
+        if y_min == None: y_min = Y[0]
+        if y_max == None: y_max = Y[-1]
+        Yn = np.arange(y_min, y_max+dy, dy)     # an array whose elements become the column look up values in the new table, where the n significes 'new' Y
+        indices_of_Yn_equal_or_above_Y = np.ceil((Y-Y[0])/dy).astype(int)    # see comment for indices_of_Xn_equal_or_above_X above
+        Zn = np.zeros((len(Xn),len(Yn)))      # an array to store the new dependent variable Z data after resampling has occured in BOTH the x (down the columns) and y (across rows) directions
+        
+        for i in range(0,len(Zn)):  # where length of a 2D array returns the number of rows
             
-    # if dy != None:
-    #     Yn = np.arange(y_min, y_max+dy, dy)      # an array whose elements become the column look up values in the new table ie the new Y (Yn) data range
-    #     # Zn = np.zeros((len(Xn), len(Yn)))        # an array to store the new resampled, dependent variable Z data
-    #     indices_of_Yn_above_Y = np.ceil((Y-Y[0])/dy).astype(int)
-    
-    ndf = pd.DataFrame(np.insert(Zn,0,Xn,axis =1),columns = df.columns)
+            dz_dy = np.diff(Znx[i,:])/np.diff(Y)
+            c = Znx[i,1:] - dz_dy*Y[1:]
+            
+            # Extrapolating
+            if y_min < Y[0]:
+                Zn[i,0:indices_of_Yn_equal_or_above_Y[0]] = dz_dy[0]*Yn[0:indices_of_Yn_equal_or_above_Y[0]]+c[0]
+            if y_max > Y[-1]:
+                Zn[i,indices_of_Yn_equal_or_above_Y[-1]:] = dz_dy[-1]*Yn[indices_of_Yn_equal_or_above_Y[-1]:]+c[-1]
+            elif y_max == Y[-1]:
+                Zn[i,-1] = Znx[i,-1]
+                
+            # Interpolating
+            for j in range(0,len(dz_dy)):
+                Zn[i, indices_of_Yn_equal_or_above_Y[j]:indices_of_Yn_equal_or_above_Y[j+1]] = dz_dy[j]*Yn[indices_of_Yn_equal_or_above_Y[j]:indices_of_Yn_equal_or_above_Y[j+1]]+c[j]
+        
+        ndf = pd.DataFrame(np.insert(Zn,0,Xn,axis=1), columns = list(df.columns[0])+list(Yn))
+        
+    else:
+        ndf = pd.DataFrame(np.insert(Znx,0,Xn,axis=1),columns = df.columns)
         
     return(ndf)
     
@@ -120,4 +150,5 @@ if __name__ == "__main__":
     new_df2 = dataframe_resampler(df, 0.1, x_max = 3.5)
     new_df3 = dataframe_resampler(df, 0.1, x_min = -0.5, x_max = 3.5)
     new_df4 = dataframe_resampler(df, 0.1, x_min = 1.5, x_max = 2.5)
+    new_df5 = dataframe_resampler(df, 0.1, x_max = 3, dy = 1, y_max = 3)
     
